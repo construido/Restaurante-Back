b778a2c3aa23bd80953cb04918826c9561d09c1a
construido <eligiovaca@gmail.com>
2022-08-09 02:50:15 -0400

    Nueva Actualizacion

--- config/app.php
+++ config/app.php
@@ -182,7 +182,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         //'Tymon\JWTAuth\Providers\JWTAuthServiceProvider',
-        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        'Tymon\JWTAuth\Providers\LaravelServiceProvider',
         /*
          * Package Service Providers...
          */
